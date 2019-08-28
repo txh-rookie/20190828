c6612450b401ea39eaa674caf45588471783bdc6
谭小虎 <843808107@qq.com>
2019-08-28 21:35:09 +0800

    20190828提交的文件

---
+++ src/main/java/cn/txh/springbootday03/SpringbootDay03Application.java
@@ -0,0 +1,13 @@
+package cn.txh.springbootday03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDay03Application{
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDay03Application.class, args);
+    }
+
+}
---
+++ src/main/java/cn/txh/springbootday03/controller/HelloController.java
@@ -0,0 +1,13 @@
+package cn.txh.springbootday03.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController{
+    @GetMapping("/hello")
+    public String hello(){
+        return "success";
+    }
+
+}
